bf04304f5c58fa6bcad898afef8714049d38583b
Rohit singh <singhrohit77443@gmail.com>
2025-03-10 12:12:13 +0530

    added files

---
+++ src/components/GameKeyboard.jsx
@@ -0,0 +1,24 @@
+const GameKeyboard = ({ handleClickedKey, clickedKeys }) => {
+    // Create an array of alphabet letters from 'a' to 'z'
+    const alphabetArray = Array.from({ length: 26 }, (_, i) =>
+      String.fromCharCode(97 + i),
+    );
+    return (
+      // Container for the keyboard buttons
+      <div className="mt-9 flex flex-wrap justify-center gap-1">
+        {alphabetArray.map((char) => (
+          <button
+            onClick={() => handleClickedKey(char)}
+            disabled={clickedKeys.includes(char)} // Disable button if key is already clicked
+            key={char}
+            className={`w-[calc(100%/9-5px)] rounded-md border bg-emerald-700 py-1.5 font-semibold text-white hover:bg-emerald-600 ${clickedKeys.includes(char) && "pointer-events-none opacity-60"}`}
+          >
+            {char.toUpperCase()}
+          </button>
+        ))}
+        <h3 className="mt-2 pt-3 font-thin text-xs">Created By Rohit Singh</h3>
+      </div>
+      
+    );
+  };
+  export default GameKeyboard;
---
+++ src/constants/index.js
@@ -0,0 +1,115 @@
+// List of words and their corresponding hints for the game
+export const wordList = [
+    {
+      word: "guitar",
+      hint: "A musical instrument with strings.",
+    },
+    {
+      word: "oxygen",
+      hint: "A colorless, odorless gas essential for life.",
+    },
+    {
+      word: "mountain",
+      hint: "A large natural elevation of the Earth's surface.",
+    },
+    {
+      word: "painting",
+      hint: "An art form using colors on a surface to create images or expression.",
+    },
+    {
+      word: "astronomy",
+      hint: "The scientific study of celestial objects and phenomena.",
+    },
+    {
+      word: "football",
+      hint: "A popular sport played with a spherical ball.",
+    },
+    {
+      word: "chocolate",
+      hint: "A sweet treat made from cocoa beans.",
+    },
+    {
+      word: "butterfly",
+      hint: "An insect with colorful wings and a slender body.",
+    },
+    {
+      word: "history",
+      hint: "The study of past events and human civilization.",
+    },
+    {
+      word: "pizza",
+      hint: "A savory dish consisting of a round, flattened base with toppings.",
+    },
+    {
+      word: "jazz",
+      hint: "A genre of music characterized by improvisation and syncopation.",
+    },
+    {
+      word: "camera",
+      hint: "A device used to capture and record images or videos.",
+    },
+    {
+      word: "diamond",
+      hint: "A precious gemstone known for its brilliance and hardness.",
+    },
+    {
+      word: "adventure",
+      hint: "An exciting or daring experience.",
+    },
+    {
+      word: "science",
+      hint: "The systematic study of the structure and behavior of the physical and natural world.",
+    },
+    {
+      word: "bicycle",
+      hint: "A human-powered vehicle with two wheels.",
+    },
+    {
+      word: "sunset",
+      hint: "The daily disappearance of the sun below the horizon.",
+    },
+    {
+      word: "coffee",
+      hint: "A popular caffeinated beverage made from roasted coffee beans.",
+    },
+    {
+      word: "dance",
+      hint: "A rhythmic movement of the body often performed to music.",
+    },
+    {
+      word: "galaxy",
+      hint: "A vast system of stars, gas, and dust held together by gravity.",
+    },
+    {
+      word: "orchestra",
+      hint: "A large ensemble of musicians playing various instruments.",
+    },
+    {
+      word: "volcano",
+      hint: "A mountain or hill with a vent through which lava, rock fragments, hot vapor, and gas are ejected.",
+    },
+    {
+      word: "novel",
+      hint: "A long work of fiction, typically with a complex plot and characters.",
+    },
+    {
+      word: "bloxfruits",
+      hint: "A popular Roblox game including fighting and exploring the sea.",
+    },
+    {
+      word: "aiku",
+      hint: "The best defender in Blue Lock anime series.",
+    },
+    {
+      word: "kunigami",
+      hint: "The best striker in Blue Lock anime series.",
+    },
+    {
+      word: "isagi",
+      hint: "The worst striker in Blue Lock anime series.",
+    },
+    {
+      word: "donlorenzo",
+      hint: "The dribbler in Blue Lock which plays like a zombie, and he's world class player.",
+    }
+  ];
